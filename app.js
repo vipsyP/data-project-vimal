b5b120c8530c0792f14819ce07b9e4866738dd66
Vimal Patel <vimal.patel@mountblue.io>
2018-06-02 18:27:56 +0530

    start using sync/await

--- app.js
+++ app.js
@@ -65,9 +65,19 @@ function populateMatchesDB() {
 
 }
 
+// find all unique years in the db
+async function findSeasons(req, res) {
+
+}
+
+// find unique winners in the db
+function findUniqueWinners(req, res) {
+
+}
+
 // find no of matches played each year--from matches database
 function findNoOfMatchesPlayed(req, res) {
-
+    console.log("Received request to /api/numberOfMatches");
     var noOfMatches = [];
     for (let i = 2008; i <= 2017; i++) {
         MatchModel.count({
@@ -89,11 +99,138 @@ function findNoOfMatchesPlayed(req, res) {
     }, 1000);
 }
 
+// find no of matches won by each team over the years--from matches database
+async function findNoOfMatchesWon(req, res) {
+    console.log("Received request to /api/stackedBarGraph");
+
+    let years = new Set();
+    //find all the unique years
+
+    let promise = new Promise(function (resolve, reject) {
+        MatchModel.find({}, {
+            '_id': 0,
+            'season': 1
+        }, function (err, docs) {
+
+            if (err) {
+                reject(error);
+            } else {
+                resolve(docs);
+            }
+        })
+    });
+
+    let result = await promise;
+    console.log("does this work?: " + result);
+
+    result.forEach(function (arrayItem) {
+        console.log("Years item: " + arrayItem);
+        if (arrayItem.season.trim() != "")
+            years.add(arrayItem.season);
+    });
+    console.log("Years length: " + years.size);
+    for (let year of years) console.log(year);
+
+
+
+
+}
+// let teams = findUniqueWinners();
+// let teams = new Set();
+// //find all the unique winners & years
+// MatchModel.find({}, {
+//     '_id': 0,
+//     'winner': 1
+// }, function (err, docs) {
+//     console.log("length of array retrieved: " + docs.length);
+//     docs.forEach(function (arrayItem) {
+//         // console.log(arrayItem.winner);
+//         if (arrayItem.winner.trim() != "")
+//             teams.add(arrayItem.winner);
+//     });
+// })
+
+
+// console.log("Teams size: " + teams.size);
+// for (let team of teams) console.log(team);
+
+// let noOfMatchesWon = [];
+// let forTheSeason = [];
+// console.log("forTheSeason: " + forTheSeason);
+
+
+// let i = 0,
+//     j = 0;
+// for (let year of years) {
+//     j=0;
+//     for (let team of teams) {
+//         console.log("J: "+j++);
+//     }
+// }
+
+
+//         let i = 0,
+//             j = 0;
+
+//         for (let year of years) {
+
+//             for (let team of teams) {
+
+
+
+//                 MatchModel.count({
+//                     season: year,
+//                     winner: team
+
+//                 }, function (err, c) {
+//                     if (err) {
+//                         console.log("count error: " + err);
+//                         return;
+//                     }
+//                     forTheSeason[j] = c;
+//                     // forTheSeason.splice(j++, 0, c); // insert c at index j, while deleting 0 items
+//                     console.log("Number of matches won by " + team + " in " + year + " : " + c);
+//                     //console.log("Value in array for year " + (i+2008).toString() + " : " + noOfMatches[i - 2008]);
+
+//                     j++;
+//                     console.log("team.size: " + teams.size);
+//                     console.log("value of j: " + j);
+//                 });
+//             } // end of inner for
+
+//             setTimeout(function () {
+//                 noOfMatchesWon[i] = forTheSeason;
+//                 //noOfMatchesWon.splice(i++, 0, forTheSeason);
+//                 //console.log(noOfMatchesWon[i]);
+
+//                 i++;
+//                 console.log("value of i: " + i);
+
+//                 j = 0;
+//                 forTheSeason = [];
+//             }, 1000);
+
+//         } // end of outer for
+//         setTimeout(function () {
+//             console.log("2D array: " + noOfMatchesWon);
+//             res.send(noOfMatchesWon);
+//         }, 1000);
+//     }, 1000);
+// }
+
 // home route
 app.get("/", (req, res) => {
     console.log("home dir!");
     res.sendFile(path.join(__dirname + 'index.html'));
 });
+
+// get number of matches played per year array
+// app.get('/api/uniqueYears', (req, res) => {
+
+//     findUniqueWinners(req, res);
+
+// })
+
 
 // get number of matches played per year array
 app.get('/api/numberOfMatches', (req, res) => {
@@ -104,8 +241,21 @@ app.get('/api/numberOfMatches', (req, res) => {
 
     // populateMatchesDB();
 
-    findNoOfMatchesPlayed(req, res)
+    findNoOfMatchesPlayed(req, res);
 
 })
 
-app.listen(3000, () => console.log('listening on port 3000!'))
+// get number of matches played per year array
+app.get('/api/stackedBarGraph', (req, res) => {
+
+    // getJSONfromCSV();
+
+    // clearMatchesDB();
+
+    // populateMatchesDB();
+
+    findNoOfMatchesWon(req, res);
+
+})
+
+app.listen(3000, () => console.log('listening on port 3000!'));
--- public/script.js
+++ public/script.js
@@ -1,29 +1,16 @@
-var chart = Highcharts.chart('container', {
-
-    title: {
-        text: 'Loading...'
-    },
-
-    // subtitle: {
-    //     text: 'Plain'
-    // },
-
-    xAxis: {
-        categories: ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']
-    },
-
-    series: [{
-        type: 'column',
-        colorByPoint: true,
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        showInLegend: false
-    }]
-
-});
-
 function load() {
+    var chart = Highcharts.chart('container', {
 
-    console.log("starting retrieve request");
+        title: {
+            text: 'Press a button to plot a chart'
+        }
+
+    });
+}
+
+
+function plotNoOfMatchesPlayed() {
+    console.log("starting request to /api/numberOfMatches");
     $.ajax({
         type: "get",
         url: 'http://localhost:3000/api/numberOfMatches',
@@ -58,5 +45,116 @@ function load() {
             });
         }
     })
-
 }
+
+function plotStackedBarGraph() {
+    console.log("starting request to /api/stackedBarGraph");
+
+    //find unique years
+    // let years = new Set();
+
+    // $.ajax({
+    //     type: "get",
+    //     url: 'http://localhost:3000/api/stackedBarGraph',
+    //     contentType: 'application/json',
+    //     success: function (items) {
+
+    //     }
+    // }); 
+
+
+    $.ajax({
+        type: "get",
+        url: 'http://localhost:3000/api/stackedBarGraph',
+        contentType: 'application/json',
+        success: function (itemsOfItems) {
+
+            console.log("count of matches won by each team over the years array: ", itemsOfItems);
+
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Stacked bar chart'
+                },
+                xAxis: {
+                    categories: ["Sunrisers Hyderabad",
+                        "Rising Pune Supergiant",
+                        "Kolkata Knight Riders",
+                        "Kings XI Punjab",
+                        "Royal Challengers Bangalore",
+                        "Mumbai Indians",
+                        "Delhi Daredevils",
+                        "Gujarat Lions",
+                        "Chennai Super Kings",
+                        "Rajasthan Royals",
+                        "Deccan Chargers",
+                        "Pune Warriors",
+                        "Kochi Tuskers Kerala",
+                        "Rising Pune Supergiantsc"
+                    ]
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Total fruit consumption'
+                    }
+                },
+                legend: {
+                    reversed: true
+                },
+                plotOptions: {
+                    series: {
+                        stacking: 'normal'
+                    }
+                },
+                series: [{
+                        name: '2008',
+                        data: itemsOfItems[0]
+                    }, {
+                        name: '2009',
+                        data: itemsOfItems[1]
+                    }, {
+                        name: '2010',
+                        data: itemsOfItems[2]
+                    },
+                    {
+                        name: '2011',
+                        data: itemsOfItems[3]
+                    },
+                    {
+                        name: '2012',
+                        data: itemsOfItems[4]
+                    },
+                    {
+                        name: '2013',
+                        data: itemsOfItems[5]
+                    },
+                    {
+                        name: '2014',
+                        data: itemsOfItems[6]
+                    },
+                    {
+                        name: '2015',
+                        data: itemsOfItems[7]
+                    },
+                    {
+                        name: '2016',
+                        data: itemsOfItems[8]
+                    },
+                    {
+                        name: '2017',
+                        data: itemsOfItems[9]
+                    }
+                ]
+            });
+        }
+    });
+}
+
+function plotExtraRunsConceded() {}
+
+function plotTopEconomicalBowlers() {}
+
+function plotStory() {}
